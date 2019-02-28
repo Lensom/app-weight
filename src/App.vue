c886bb4b5d5b276101706715f139aa448d06b4b3
unknown <magurawork@gmail.com>
2019-02-28 11:46:23 +0300

    return fonts and style

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,65 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import MainPage from './components/MainPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    // HelloWorld,
     MainPage
   }
 }
 </script>
 
 <style>
+* {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+*:before,
+*:after {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+*::-webkit-input-placeholder,
+*:-moz-placeholder,
+*::-moz-placeholder,
+*:-ms-input-placeholder {
+  color: #666;
+  opacity: 1;
+}
+body input:focus:required:invalid,
+body textarea:focus:required:invalid {
+  color: #666;
+}
+body input:required:valid,
+body textarea:required:valid {
+  color: #666;
+}
+html,
+body {
+  height: 100%;
+}
+body {
+  font-size: 16px;
+  min-width: 320px;
+  position: relative;
+  line-height: 1.65;
+  font-family: "RobotoBold", sans-serif;
+  overflow-x: hidden;
+  margin: 0;
+}
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+.hide {
+  display: none;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,4 +73,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
